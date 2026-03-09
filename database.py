541ba78b8f78b7ec9b1089261e4e450237652613
SouthSun425 <namtaeyang@gmail.com>
2026-03-09 20:49:39 +0900

    Remove debug print

--- database.py
+++ database.py
@@ -6,8 +6,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL 환경변수가 설정되지 않았습니다.")
 
-print("DATABASE_URL:", DATABASE_URL)
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
